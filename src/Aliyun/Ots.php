52523d79deb97a62ad6748147e27f99515f8ea83
zhaochaoxian <613600010@qq.com>
2020-08-27 22:17:06 +0800

    Update Ots.php

--- src/Aliyun/Ots.php
+++ src/Aliyun/Ots.php
@@ -1,2 +1,24 @@
+<?php
+namespace Yycms/Aliyun;
+use Aliyun\OTS\OTSServerException;
+
+class Ots{
+    
+	public static function __callStatic($method,$parameters){  
 		Vendor('vendor.autoload');
+		$otsClient = new \Aliyun\OTS\OTSClient(array(
 	        'EndPoint' => C('ALIYUN.OTS_APIURL'),
+	        'AccessKeyID' => C('ALIYUN.ACCESS_KEY'),
+	        'AccessKeySecret' => C('ALIYUN.ACCESS_SECRET'),
+	        'InstanceName' => C('ALIYUN.OTS_NAME'),
+	        'DebugLogHandler'=>false,
+	        'ErrorLogHandler'=>false
+	    ));
+	    
+		try {
+		    return $otsClient->$method(...$parameters);
+		} catch (OTSServerException $e) {
+			return ['code'=>$e->getOTSErrorCode(),'msg'=>$e->getOTSErrorMessage()];
+		}
+  	}
+}
