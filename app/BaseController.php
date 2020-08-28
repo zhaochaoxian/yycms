3ee0fed2daf9f0a2b048057abfae75966cd051a4
zhaochaoxian <613600010@qq.com>
2020-08-28 21:35:19 +0800

    Create BaseController.php

---
+++ app/BaseController.php
@@ -0,0 +1,174 @@
+<?php
+declare (strict_types = 1);
+
+namespace app;
+
+use think\App;
+use think\exception\ValidateException;
+use think\Validate;
+use think\facade\View;
+
+/**
+ * 控制器基础类
+ */
+abstract class BaseController
+{
+    /**
+     * Request实例
+     * @var \think\Request
+     */
+    protected $request;
+
+    /**
+     * 应用实例
+     * @var \think\App
+     */
+    protected $app;
+
+    /**
+     * 是否批量验证
+     * @var bool
+     */
+    protected $batchValidate = false;
+
+    /**
+     * 控制器中间件
+     * @var array
+     */
+    protected $middleware = [];
+
+    /**
+     * 构造方法
+     * @access public
+     * @param  App  $app  应用对象
+     */
+    public function __construct(App $app)
+    {
+        $this->app     = $app;
+        $this->request = $this->app->request;
+
+        // 控制器初始化
+        $this->initialize();
+    }
+
+    // 初始化
+    protected function initialize(){}
+    
+    public function __call($method, $args){
+        return '方法不存在!';
+    }
+
+    /**
+     * 验证数据
+     * @access protected
+     * @param  array        $data     数据
+     * @param  string|array $validate 验证器名或者验证规则数组
+     * @param  array        $message  提示信息
+     * @param  bool         $batch    是否批量验证
+     * @return array|string|true
+     * @throws ValidateException
+     */
+    protected function validate(array $data, $validate, array $message = [], bool $batch = false)
+    {
+        if (is_array($validate)) {
+            $v = new Validate();
+            $v->rule($validate);
+        } else {
+            if (strpos($validate, '.')) {
+                // 支持场景
+                list($validate, $scene) = explode('.', $validate);
+            }
+            $class = false !== strpos($validate, '\\') ? $validate : $this->app->parseClass('validate', $validate);
+            $v     = new $class();
+            if (!empty($scene)) {
+                $v->scene($scene);
+            }
+        }
+
+        $v->message($message);
+
+        // 是否批量验证
+        if ($batch || $this->batchValidate) {
+            $v->batch(true);
+        }
+
+        return $v->failException(true)->check($data);
+    }
+    
+    /**
+     * 操作错误跳转的快捷方法
+     * @access protected
+     * @param string $message 错误信息
+     * @param string $jumpUrl 页面跳转地址
+     * @param mixed $ajax 是否为Ajax方式 当数字时指定跳转时间
+     * @return void
+     */
+    protected function error($message='',$jumpUrl='',$ajax=false) {
+        return $this->dispatchJump($message,0,$jumpUrl,$ajax);
+    }
+
+    /**
+     * 操作成功跳转的快捷方法
+     * @access protected
+     * @param string $message 提示信息
+     * @param string $jumpUrl 页面跳转地址
+     * @param mixed $ajax 是否为Ajax方式 当数字时指定跳转时间
+     * @return void
+     */
+    protected function success($message='',$jumpUrl='',$ajax=false) {
+        return $this->dispatchJump($message,1,$jumpUrl,$ajax);
+    }
+    
+    /**
+     * 默认跳转操作 支持错误导向和正确跳转
+     * 调用模板显示 默认为public目录下面的success页面
+     * 提示页面为可配置 支持模板标签
+     * @param string $message 提示信息
+     * @param Boolean $status 状态
+     * @param string $jumpUrl 页面跳转地址
+     * @param mixed $ajax 是否为Ajax方式 当数字时指定跳转时间
+     * @access private
+     * @return void
+     */
+    private function dispatchJump($message,$status=1,$jumpUrl='',$ajax=false) {
+        if(true === $ajax || request()->isAjax()) {// AJAX提交
+            $data           =   is_array($ajax)?$ajax:array();
+            $data['code']    =   $status;
+            $data['msg']   =   $message;
+            $data['url']    =   $jumpUrl;
+            return json($data);
+        }
+        if(is_int($ajax)) $this->assign('waitSecond',$ajax);
+        if(!empty($jumpUrl)) $this->assign('jumpUrl',$jumpUrl);
+        // 提示标题
+        $this->assign('msgTitle',$status? L('_OPERATION_SUCCESS_') : L('_OPERATION_FAIL_'));
+        //如果设置了关闭窗口，则提示完毕后自动关闭窗口
+        if($this->get('closeWin'))    $this->assign('jumpUrl','javascript:window.close();');
+        $this->assign('status',$status);   // 状态
+        //保证输出不受静态缓存影响
+        C('HTML_CACHE_ON',false);
+        if($status) { //发送成功信息
+            $this->assign('message',$message);// 提示信息
+            // 成功操作后默认停留1秒
+            if(!isset($this->waitSecond))    $this->assign('waitSecond','1');
+            // 默认操作成功自动返回操作前页面
+            if(!isset($this->jumpUrl)) $this->assign("jumpUrl",$_SERVER["HTTP_REFERER"]);
+            $this->display(C('TMPL_ACTION_SUCCESS'));
+        }else{
+            $this->assign('error',$message);// 提示信息
+            //发生错误时候默认停留3秒
+            if(!isset($this->waitSecond))    $this->assign('waitSecond','3');
+            // 默认发生错误的话自动返回上页
+            if(!isset($this->jumpUrl)) $this->assign('jumpUrl',"javascript:history.back(-1);");
+            $this->display(C('TMPL_ACTION_ERROR'));
+            // 中止执行  避免出错后继续执行
+            exit ;
+        }
+    }
+    
+    
+    protected function assign($name,$value){
+    	return View::assign($name,$value);
+    }
+
+}
